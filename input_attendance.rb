442d51f20cb205944e77e4796a11f3acea2d01f7
Ryosuke Watanabe <suzukai74@gmail.com>
2023-11-01 10:58:48 +0900

    Fix holiday

--- input_attendance.rb
+++ input_attendance.rb
@@ -3,10 +3,10 @@ require 'yaml'
 require 'pry'
 require 'active_support/time'
 
-KINTAI_LINK_CLASS  = 'wt-勤務表'
-BASE_START_ID      = 'ttvTimeSt'
-INPUT_DIALOG_ID    = 'dijit_DialogUnderlay_0'
-YEAR_MONTH_LIST_ID = 'yearMonthList'
+KINTAI_LINK_CLASS  = 'wt-勤務表'.freeze
+BASE_START_ID      = 'ttvTimeSt'.freeze
+INPUT_DIALOG_ID    = 'dijit_DialogUnderlay_0'.freeze
+YEAR_MONTH_LIST_ID = 'yearMonthList'.freeze
 
 config       = YAML.load_file('config.yml')
 profile_path = config.key?('chrome_profile') ? config['chrome_profile'] : './chrome_profile'
@@ -30,7 +30,7 @@ begin
   wait = Selenium::WebDriver::Wait.new(timeout: 30)
   wait.until { driver.title.start_with?('Salesforce') }
   puts('Login success.')
-rescue
+rescue StandardError
   puts('Login failed.')
   driver.quit
 end
@@ -44,28 +44,31 @@ year_month_list.select_by(:value, start_date.strftime('%Y%m%d'))
 wait = Selenium::WebDriver::Wait.new(timeout: 10)
 wait.until { !driver.find_element(:id, 'shim').displayed? }
 
+def input_time(driver, id, val)
+  input = driver.find_element(:id, id)
+  input.clear
+  wait = Selenium::WebDriver::Wait.new(timeout: 10)
+  wait.until { input.text.blank? }
+  sleep 0.5 if id == 'startTime' # wait until inputable st_input
+  input.send_keys(val)
+end
+
 (start_date..end_date).each do |date|
-  begin
-    puts(date)
-    driver.find_element(:id, "#{BASE_START_ID}#{date}").click
-    wait = Selenium::WebDriver::Wait.new(timeout: 10)
-    wait.until { driver.find_element(:id, INPUT_DIALOG_ID).displayed? }
-    st_input = driver.find_element(:id, 'startTime')
-    et_input = driver.find_element(:id, 'endTime')
-    time_submit = driver.find_element(:id, 'dlgInpTimeOk')
-    st_input.clear
-    wait = Selenium::WebDriver::Wait.new(timeout: 10)
-    wait.until { st_input.text.blank? }
-    sleep 0.5 # wait until inputable st_input
-    st_input.send_keys(config['start_time'])
-    et_input.clear
-    wait = Selenium::WebDriver::Wait.new(timeout: 10)
-    wait.until { et_input.text.blank? }
-    et_input.send_keys(config['end_time'])
-    time_submit.click
-    wait = Selenium::WebDriver::Wait.new(timeout: 10)
-    wait.until { !driver.find_element(:id, INPUT_DIALOG_ID).displayed? }
-  rescue => e
-    puts(e)
+  if driver.find_elements(:id, "#{BASE_START_ID}#{date}").empty?
+    puts("#{date}: is Holiday")
+    next
   end
+  driver.find_element(:id, "#{BASE_START_ID}#{date}").click
+  wait = Selenium::WebDriver::Wait.new(timeout: 10)
+  wait.until { driver.find_element(:id, INPUT_DIALOG_ID).displayed? }
+  time_submit = driver.find_element(:id, 'dlgInpTimeOk')
+  input_time(driver, 'startTime', config['start_time'])
+  input_time(driver, 'endTime', config['end_time'])
+  time_submit.click
+  wait = Selenium::WebDriver::Wait.new(timeout: 10)
+  wait.until { !driver.find_element(:id, INPUT_DIALOG_ID).displayed? }
+  puts("#{date}:Success")
+rescue StandardError => e
+  puts("#{date}:Failure")
+  puts(e)
 end
